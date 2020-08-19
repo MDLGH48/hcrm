366307deee76d52862f1847b6c9a1940505e1638
ML <>
2020-08-19 21:15:58 +0300

    a little help from a friend2

--- kols_runconfig.sh
+++ kols_runconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export MONGO_DB_URI="mongodb+srv://dbuser:dbpass@crm-template.rrvgs.mongodb.net/<dbname>?retryWrites=true&w=majority"
+export MONGO_DB_URI="mongodb+srv://dbuser:dbpass@blah.blah.mongodb.net/<dbname>?retryWrites=true&w=majority"
 export SECRET_KEY="KOLSKEY124"
 python app.py
